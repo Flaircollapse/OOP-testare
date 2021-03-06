24499965a6168ffa415b8cf4886780df70afd031
Preecha Häll <preecha.hall@gmail.com>
2021-03-06 14:26:57 +0100

    updatering

---
+++ src/oop/testare/wallet.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oop.testare;
+
+/**
+ *
+ * @author preec
+ */
+public class wallet {
+    static int denomination1;
+    static int denomination5;
+    static int denomination10;
+    
+    void setUserMoney(){
+        //Initialisering av denominations
+        denomination1 = 10;
+        denomination5 = 10;
+        denomination10 = 10;
+    }
+    //Updatera användarens pengar
+    boolean updateUserMoney(int denomination, int numOfDenomination){
+        if (denomination == 1){
+            denomination1 -= numOfDenomination;
+            if (denomination1 < 0){
+                System.out.println("This number of 1£ denomination not available");
+                denomination1 += numOfDenomination;
+                return false;
+            }
+            System.out.println("Your Remaining Of Denomination Are:");
+            System.out.println("Denomination 1£: " + denomination1);
+            System.out.println("Denomination 5£: " + denomination5);
+            System.out.println("Denomination 10£: " + denomination10);
+            return true;
+        }else if (denomination == 5){
+            denomination5 -= numOfDenomination;
+            if (denomination5 < 0){
+                System.out.println("This number of 5£ denomination not available");
+                denomination5 += numOfDenomination;
+                return false;
+            }
+            System.out.println("Your Remaining Of Denomination Are:");
+            System.out.println("Denomination 1£: " + denomination1);
+            System.out.println("Denomination 5£: " + denomination5);
+            System.out.println("Denomination 10£: " + denomination10);
+            return true;
+        }else if (denomination == 10){
+            denomination10 -= numOfDenomination;
+            if (denomination10 < 0){
+                System.out.println("This number of 10£ denomination not available");
+                denomination10 += numOfDenomination;
+                return false;
+            }
+            System.out.println("Your Remaining Of Denomination Are:");
+            System.out.println("Denomination 1£: " + denomination1);
+            System.out.println("Denomination 5£: " + denomination5);
+            System.out.println("Denomination 10£: " + denomination10);
+            return true;
+        }
+        return false;
+    }    
+}
