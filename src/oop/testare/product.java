840bc435fa63ad481f61e1afabf81a21f22341fb
Preecha Häll <preecha.hall@gmail.com>
2021-03-06 14:25:48 +0100

    updatering

---
+++ src/oop/testare/product.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oop.testare;
+
+/**
+ *
+ * @author preec
+ */
+public abstract class product {
+    String name, description;
+    double cost;
+    
+    abstract void Description();
+    abstract void Buy();
+    abstract void Use();
+}
